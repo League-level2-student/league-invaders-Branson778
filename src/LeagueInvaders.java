004d809a7cacaa23692f1b99a2067eeef9c7bda9
student <student@ip-172-31-55-149.us-west-2.compute.internal>
2021-05-29 17:58:14 -0700

    Finish League Invaders - Text - Part 4-6

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,18 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+ JFrame frame = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel gamePanel = new GamePanel();
+	public static void main(String[] args) {
+		LeagueInvaders runner = new LeagueInvaders();
+		runner.setup();
+	}
+	void setup() {
+		frame.add(gamePanel);
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
